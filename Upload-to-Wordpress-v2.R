cb4122bb9f1139b00d67467716a248002ef92d2c
John Yuill <john@fig4.com>
2021-08-02 12:07:23 -0700

    getting it working

--- Upload-to-Wordpress-v2.R
+++ Upload-to-Wordpress-v2.R
@@ -3,7 +3,7 @@
 library(RWordPress)
 library(knitr)
 
-source('creds.R')
+source('credite.R')
 
 options(WordpressLogin= c(johnyuill=wp_p),
        WordpressURL=paste0('https://',wp_blog,'.wordpress.com/xmlrpc.php'))
@@ -13,15 +13,15 @@ options(WordpressLogin= c(johnyuill=wp_p),
 ## - saves files in a folder in WordPress
 ## http://catbirdanalytics.files.wordpress.com/YYYY/MM/<name of code chunk>-1.png
 ## throws Warnings about deprecated functions :(
-#opts_knit$set(upload.fun = function(file){library(RWordPress);uploadFile(file)$url;})
+opts_knit$set(upload.fun = function(file){library(RWordPress);uploadFile(file)$url;})
 
-knit2wp('test-r-markdown-wordpress-v2.Rmd', ## markdown file to publish 
-        title = "test markdown to wp - v2", ## title for the post in WordPress 
+knit2wp('R-Markdown-WordPress-v4.Rmd', ## markdown file to publish 
+        title = "R Markdown to WP? Yes You Can!", ## title for the post in WordPress 
         publish = FALSE, ## FALSE to add as draft; TRUE to go direct to publish
-        action = "newPost", ## for new post; alternatives: "editPost", "newPage"
-        #postid ## needed with editPost - get from WP interface
-        shortcode= TRUE, ## not clear what this does
-        categories=c('R Markdown')) ## set categories
+        action = "editPost", # "newPost", ## for new post; alternatives: "editPost", "newPage"
+        postid='816', ## needed with editPost - get from WP interface
+        shortcode= FALSE, ## affects how source code is displayed; default is FALSE
+        categories=c('R Stats','R Markdown')) ## associate with categories (need to be added first)
 
 ## alternative approach to figures: [DOESN'T WORK!]
 ## 1. use ggsave to save charts with desired name/location locally (at least for ggplot2)
