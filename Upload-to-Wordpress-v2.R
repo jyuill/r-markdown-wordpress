818b13630997012eeb7b4acd4ad08fd7551740a7
John Yuill <john@fig4.com>
2021-08-02 18:13:39 -0700

    finalizing working version

--- Upload-to-Wordpress-v2.R
+++ Upload-to-Wordpress-v2.R
@@ -3,27 +3,37 @@
 library(RWordPress)
 library(knitr)
 
+## set up values needed
 source('credite.R')
+blogthumbnail <- "figure/temperature-plot-1.png"
+blogfile <- "R-Markdown-WordPress.Rmd"
+blogtitle <- "Publish R Markdown to WordPress site? Yes You Can!"
+blogaction <- "newPost" ## "newPost", "editPost", "newPage"
+blogpostid <- ""
+blogcat <- c('R Stats', 'R Markdown')
 
 options(WordpressLogin= c(johnyuill=wp_p),
        WordpressURL=paste0('https://',wp_blog,'.wordpress.com/xmlrpc.php'))
 
+# Include toc (comment out if not needed)
+## this works but uses ALL headings - don't know how to limit
+#library(markdown)
+#options(markdown.HTML.options = c(markdownHTMLOptions(default = T),"toc"))
+
 # Upload plots: set knitr options
-## this works but:
 ## - saves files in a folder in WordPress
-## http://catbirdanalytics.files.wordpress.com/YYYY/MM/<name of code chunk>-1.png
-## throws Warnings about deprecated functions :(
+## can be found in Media section by date; will be named: <name of code chunk>-1.png
+## - can edit caption, alt text, description, get URL
 opts_knit$set(upload.fun = function(file){library(RWordPress);uploadFile(file)$url;})
 
-knit2wp('R-Markdown-WordPress-v4.Rmd', ## markdown file to publish 
-        title = "R Markdown to WP? Yes You Can!", ## title for the post in WordPress 
-        publish = FALSE, ## FALSE to add as draft; TRUE to go direct to publish
-        action = "editPost", # "newPost", ## for new post; alternatives: "editPost", "newPage"
-        postid='816', ## needed with editPost - get from WP interface
-        shortcode= FALSE, ## affects how source code is displayed; default is FALSE
-        categories=c('R Stats','R Markdown')) ## associate with categories (need to be added first)
+# Upload featured image / post thumbnail: option: wp_post_thumbnail=postThumbnail$id
+postThumbnail <- RWordPress::uploadFile(blogthumbnail,overwrite = TRUE)
 
-## alternative approach to figures: [DOESN'T WORK!]
-## 1. use ggsave to save charts with desired name/location locally (at least for ggplot2)
-## 2. use googledrive to upload file to GDrive
-## 3. embed link to GDrive location
+knit2wp(blogfile, ## markdown file to publish 
+        title = blogtitle, ## title for the post in WordPress 
+        publish = FALSE, ## FALSE to add as draft; TRUE to go direct to publish
+        action = blogaction, # "newPost" for new post; alternatives: "editPost", "newPage"
+        #postid=blogpostid, ## needed with editPost - get from WP interface
+        shortcode= FALSE, ## affects how source code is displayed; default is FALSE
+        categories=blogcat, ## categories for blog post (need to be added first)
+        wp_post_thumbnail=postThumbnail$id) 
