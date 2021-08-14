bc6a97cf16554122369f1290e61b67f9aed37add
John Yuill <john@fig4.com>
2021-08-14 13:26:35 -0700

    updating template test info

---
+++ template-pkg-load-test.R
@@ -0,0 +1,15 @@
+
+## 1. INSTALL
+library(devtools)
+## from LOCAL
+install("../templateRmd")
+
+## from GITHUB
+install_github(repo="jyuill/templateRmd")
+
+## 2. LOAD
+library(templateRmd)
+
+## NOTE:
+# if NEW package or NEWLY changed package, may have to close/re-open R session
+
